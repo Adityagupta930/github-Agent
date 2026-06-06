e513ad797efe24e459767e1da726b68ddaec4b5b
Aditya Gupta <121934082+Adityagupta930@users.noreply.github.com>
2026-06-06 22:42:49 +0530

    bug fixed

--- file_handler.py
+++ file_handler.py
@@ -1,8 +1,8 @@
 import os
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def append_newline(filepath):
+    
     safe_path = os.path.realpath(os.path.join(BASE_DIR, filepath))
     if not safe_path.startswith(BASE_DIR):
         raise ValueError("Invalid file path")
